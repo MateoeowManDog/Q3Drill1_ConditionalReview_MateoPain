7bf869552f356a5cd1e0ba65f9ba9418175a1b04
MateoeowManDog <painadler@gmail.com>
2026-01-05 09:07:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from pyscript import document
-
+#the funtion of the code, this is to find the average of two scores
 def find_average(event=None):
     s1 = int(document.getElementById("score1").value or 0)
     s2 = int(document.getElementById("score2").value or 0)
 
-    avg = (s1 + s2) / 2
+    avg = (s1 + s2) / 2 #this is how the code computes the average
     if avg >= 75:
      msg =f"Average: {avg:.2f} Passed? yes!✅ " 
 
